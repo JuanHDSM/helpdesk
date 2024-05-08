9d600191c3e0881a2716c0799e5611c523ba44a2
JuanHoly <juanholydesouza@gmail.com>
2024-05-07 22:07:59 -0400

    feat: add annotation jpa

--- src/main/java/com/juanholy/helpdesk/domain/Technician.java
+++ src/main/java/com/juanholy/helpdesk/domain/Technician.java
@@ -1,6 +1,9 @@
 package com.juanholy.helpdesk.domain;
 
 import com.juanholy.helpdesk.domain.enums.Profile;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -13,10 +16,13 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Technician extends User {
 
 
-    private List<Request> requests =  new ArrayList<>();
+    @OneToMany(mappedBy = "technician")
+    private List<Request> requests = new ArrayList<>();
 
     public Technician(Long id, String name, @CPF String cpf, String email, String password, Set<Profile> profile, LocalDate createDate, List<Request> requests) {
         super(id, name, cpf, email, password, profile, createDate);
