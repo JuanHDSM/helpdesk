7162b2dfe626451ae422ed1a032d1675f29dceb3
JuanHoly <juanholydesouza@gmail.com>
2024-05-08 17:44:18 -0400

    feat: create profiles dev and test

--- src/main/java/com/juanholy/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/juanholy/helpdesk/HelpdeskApplication.java
@@ -1,43 +1,12 @@
 package com.juanholy.helpdesk;
 
-import com.juanholy.helpdesk.domain.Client;
-import com.juanholy.helpdesk.domain.Request;
-import com.juanholy.helpdesk.domain.Technician;
-import com.juanholy.helpdesk.domain.enums.Priority;
-import com.juanholy.helpdesk.domain.enums.Profile;
-import com.juanholy.helpdesk.domain.enums.Status;
-import com.juanholy.helpdesk.repositories.ClientRepository;
-import com.juanholy.helpdesk.repositories.RequestRepository;
-import com.juanholy.helpdesk.repositories.TechnicianRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-	@Autowired
-	private TechnicianRepository technicianRepository;
-	@Autowired
-	private ClientRepository clientRepository;
-	@Autowired
-	private RequestRepository requestRepository;
-
+public class HelpdeskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Technician tec1 = new Technician(null, "Valdir Cezar", "44023016020", "valdir@gmail.com", "123");
-		tec1.addProfile(Profile.ADMIN);
-		technicianRepository.save(tec1);
-
-		Client cli1 =  new Client(null, "Linus Torvalds", "44784557083", "torvalds@gmail.com", "123");
-		clientRepository.save(cli1);
-
-		Request req = new Request(null, null, Priority.MEDIUM, Status.IN_PROGRESS, "Request 1", "First request", tec1, cli1);
-		requestRepository.save(req);
-	}
 }
---
+++ src/main/java/com/juanholy/helpdesk/TestConfig.java
@@ -0,0 +1,39 @@
+package com.juanholy.helpdesk;
+
+import com.juanholy.helpdesk.domain.Client;
+import com.juanholy.helpdesk.domain.Request;
+import com.juanholy.helpdesk.domain.Technician;
+import com.juanholy.helpdesk.domain.enums.Priority;
+import com.juanholy.helpdesk.domain.enums.Status;
+import com.juanholy.helpdesk.repositories.ClientRepository;
+import com.juanholy.helpdesk.repositories.RequestRepository;
+import com.juanholy.helpdesk.repositories.TechnicianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private TechnicianRepository technicianRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private RequestRepository requestRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+        Technician tec1 = new Technician(null, "Valdir Cezar", "44023016020", "valdir@gmail.com", "123");
+        tec1.addProfile(com.juanholy.helpdesk.domain.enums.Profile.TECHNICIAN);
+        technicianRepository.save(tec1);
+
+        Client cli1 =  new Client(null, "Linus Torvalds", "44784557083", "torvalds@gmail.com", "123");
+        clientRepository.save(cli1);
+
+        Request req = new Request(null, null, Priority.MEDIUM, Status.IN_PROGRESS, "Request 1", "First request", tec1, cli1);
+        requestRepository.save(req);
+    }
+}
